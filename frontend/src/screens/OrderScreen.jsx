d646344eb8c85da702d67552b11d424b0aeabdde
vigneshv1cky <97196163+vigneshv1cky@users.noreply.github.com>
2025-02-18 23:36:59 -0600

    changed currecy to USD

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -45,7 +45,7 @@ const OrderScreen = () => {
           type: 'resetOptions',
           value: {
             'client-id': paypal.clientId,
-            currency: 'INR',
+            currency: 'USD',
           },
         });
         paypalDispatch({ type: 'setLoadingStatus', value: 'pending' });
